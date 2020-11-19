434719decf246cc67b49f01417058084d0659335
key kim <keyhyuk.kim@gmail.com>
2020-11-19 22:18:54 +0900

    Change as callalbd

--- spark_submit.dag.py
+++ spark_submit.dag.py
@@ -10,20 +10,20 @@ dag = DAG(dag_id='test',
 def greeting(*args):
     import logging 
     logging.info(args)
-
+python_callable=my_func, op_args=['one', 'two', 'three']
 # An instance of an operator is called a task. In this case, the
 # hello_python task calls the "greeting" Python function.
 taks_1 = python_operator.PythonOperator(
     task_id='start',
-    python_callable=greeting('start'))
+    python_callable=greeting, op_args=['start'])
 taks_2_1 = python_operator.PythonOperator(
     task_id='mid_1',
-    python_callable=greeting('mid_1'))
+    python_callable=greeting, op_args=['mid_1'])
 taks_2_2 = python_operator.PythonOperator(
     task_id='mid_2',
-    python_callable=greeting('mid_2'))
+    python_callable=greeting, op_args=['mid_2'])
 taks_3 = python_operator.PythonOperator(
     task_id='end',
-    python_callable=greeting('end'))
+    python_callable=greeting, op_args=['end'])
 
 taks_1 >> [taks_2_1, taks_2_2] >> taks_3
