2afc5069d5da49ccdbcb15d47bd2b82710bbb6b7
key kim <keyhyuk.kim@gmail.com>
2020-11-28 12:38:55 +0900

    [WIP] example

--- dags/emr_dag.py
+++ dags/emr_dag.py
@@ -1,20 +1,33 @@
-from airflow.operators import python_operator
-from airflow import DAG
-from airflow.utils.dates import days_ago
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
+This is an example dag for a AWS EMR Pipeline.
+Starting by creating a cluster, adding steps/operations, checking steps and finally when finished
+terminating the cluster.
+"""
 from datetime import timedelta
+
 from airflow import DAG
 from airflow.providers.amazon.aws.operators.emr_add_steps import EmrAddStepsOperator
 from airflow.providers.amazon.aws.operators.emr_create_job_flow import EmrCreateJobFlowOperator
 from airflow.providers.amazon.aws.operators.emr_terminate_job_flow import EmrTerminateJobFlowOperator
 from airflow.providers.amazon.aws.sensors.emr_step import EmrStepSensor
 from airflow.utils.dates import days_ago
-
-
-import boto3
-import os
-import logging
-
-LOGGER = logging.getLogger("airflow.emr")
 
 DEFAULT_ARGS = {
     'owner': 'airflow',
@@ -26,7 +39,7 @@ DEFAULT_ARGS = {
 
 SPARK_STEPS = [
     {
-        'Name': 'process-batch',
+        'Name': 'process-avro',
         'ActionOnFailure': 'CONTINUE',
         'HadoopJarStep': {
                 'Jar': 'command-runner.jar',
@@ -45,57 +58,38 @@ SPARK_STEPS = [
                     's3a://spark-data-vjal1251/result']
         }
     }
+
 ]
 
 JOB_FLOW_OVERRIDES = {
-    'Name': 'BatchFlow',
+    'Name': 'ParseSQL',
     'ReleaseLabel': 'emr-5.27.0',
     'Instances': {
         'InstanceGroups': [
             {
-                "Name": "Master",
-                "Market": "SPOT",
-                "InstanceRole": "MASTER",
-                "InstanceType": "m4.large",
-                "InstanceCount": 1,
-            },
-            {
-                "Name": "Slave",
-                "Market": "SPOT", # Spot instances are a "use as available" instances
-                "InstanceRole": "CORE",
-                "InstanceType": "m4.large",
-                "InstanceCount": 2,
-            },
+                'Name': 'Master node',
+                'Market': 'SPOT',
+                'InstanceRole': 'MASTER',
+                'InstanceType': 'm1.medium',
+                'InstanceCount': 1,
+            }
         ],
         'KeepJobFlowAliveWhenNoSteps': True,
         'TerminationProtected': False,
     },
     'JobFlowRole': 'EMR_EC2_DefaultRole',
     'ServiceRole': 'EMR_DefaultRole',
-    'Applications': [
-        {"Name": "Hadoop"}, 
-        {"Name": "Spark"}
-    ],
 }
-
-def start():
-    LOGGER.info(os.environ)
-    LOGGER.info('start flow')
-
 
 with DAG(
     dag_id='emr_job_flow_manual_steps_dag',
     default_args=DEFAULT_ARGS,
     dagrun_timeout=timedelta(hours=2),
     start_date=days_ago(1),
-    schedule_interval=timedelta(days=1),
+    schedule_interval='0 3 * * *',
     tags=['example'],
 ) as dag:
 
-    taks_start = python_operator.PythonOperator(
-        task_id='start',
-        python_callable=start)
-        
     # [START howto_operator_emr_manual_steps_tasks]
     cluster_creator = EmrCreateJobFlowOperator(
         task_id='create_job_flow',
@@ -124,9 +118,5 @@ with DAG(
         aws_conn_id='aws_default',
     )
 
-    taks_start >> cluster_creator >> step_adder >> step_checker >> cluster_remover
+    cluster_creator >> step_adder >> step_checker >> cluster_remover
     # [END howto_operator_emr_manual_steps_tasks]
-
-
-
-
