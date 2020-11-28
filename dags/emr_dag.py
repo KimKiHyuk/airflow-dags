d27040ed3131af8258408adf84d64ddd5fd362ed
key kim <keyhyuk.kim@gmail.com>
2020-11-28 21:43:29 +0900

    Add s3 log

--- dags/emr_dag.py
+++ dags/emr_dag.py
@@ -1,25 +1,3 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-"""
-This is an example dag for a AWS EMR Pipeline.
-Starting by creating a cluster, adding steps/operations, checking steps and finally when finished
-terminating the cluster.
-"""
 from datetime import timedelta
 from airflow import DAG
 from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
@@ -66,6 +44,7 @@ SPARK_STEPS = [
 JOB_FLOW_OVERRIDES = {
     'Name': 'ParseSQL',
     'ReleaseLabel': 'emr-5.27.0',
+    "LogUri": 's3://aws-logs-417699346993-us-east-2/elasticmapreduce/',
     'Instances': {
         'InstanceGroups': [
             {
